e6ee1e20bac3f321a2202020910eca920d0cc620
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-03-25 15:58:23 -0400

    chore: remove template files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,6 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
 import * as $DropZone from "./islands/DropZone.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
@@ -16,7 +15,6 @@ const manifest = {
     "./routes/index.tsx": $index,
   },
   islands: {
-    "./islands/Counter.tsx": $Counter,
     "./islands/DropZone.tsx": $DropZone,
   },
   baseUrl: import.meta.url,
